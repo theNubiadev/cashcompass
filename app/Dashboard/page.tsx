506dd07e38a26bcb29dd39227b232a6e684af9f3
theNubiadev <ajaiyeobajibola@gmail.com>
2025-11-24 22:09:48 +0100

    Add UI components: Dashboard button, Carousel, Dropdown menu, Form elements, Popover, Spinner, Table, and Tabs

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import Link from "next/link";
 import { Navigation } from "@/components/Navbar";
-
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 export default function Dashboard() {
   return (
     <>
       <Navigation />
-      <div className="">
+      <div className="p-4">
         <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
           <div className="mb-8">
             <h1 className="text-3xl font-bold mb-2">Dashboard</h1>
@@ -14,6 +16,13 @@ export default function Dashboard() {
               Overview of your expenses and budgets
             </p>
           </div>
+
+          <Button asChild>
+            <Link href="/add-expense" className="flex items-center gap-2">
+              <PlusCircle className="h-4 w-4 " />
+              Add Expense
+            </Link>
+          </Button>
         </div>
       </div>
     </>
